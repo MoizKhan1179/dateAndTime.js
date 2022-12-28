a86bc4cddd3e6b2c9d149498199f8842ade9592e
Abdul Moiz <120182920+MoizKhan1179@users.noreply.github.com>
2022-12-28 17:07:32 +0500

    Add files via upload

---
+++ date.js
@@ -0,0 +1,56 @@
+
+// **********Date Object**********
+
+// let currentDate = new Date("July 30 2004");
+// let currentDate = new Date();
+// console.log(currentDate);
+
+
+// *********toDateString**********
+
+// let a = currentDate.toDateString()
+// console.log()
+
+
+// ************getDate***********
+
+// console.log(currentDate.getDate())
+// console.log(`After : `+ 31 - currentDate.getDate())
+
+// ********ToDateString*******
+
+// console.log(currentDate.getMonth())
+
+// console.log(currentDate.getDay())
+
+// console.log(currentDate.getHours())
+
+// console.log(currentDate.getMinutes())
+
+// console.log(currentDate.getSeconds())
+
+// console.log(currentDate.getMilliseconds())
+
+
+
+
+
+// **********SetDate**********
+
+// currentDate.setDate(2)
+// console.log(currentDate)
+
+// currentDate.setMonth(1)
+// console.log(currentDate)
+
+// currentDate.setHours(6)
+// console.log(currentDate)
+
+// currentDate.setMinutes(5)
+// console.log(currentDate)
+
+// currentDate.setMinutes(30)
+// console.log(currentDate)
+
+// currentDate.setMilliseconds(300)
+// console.log(currentDate)
